6c17090638fc7e9646a2f2c3f2df3074804e8dce
Shrawani Joshi <shrawanijoshi01@gmail.com>
2026-07-12 12:34:21 +0530

    Connect to Railway PostgreSQL

--- database.py
+++ database.py
@@ -3,11 +3,7 @@ import psycopg2
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-conn = psycopg2.connect(
-    DATABASE_URL,
-    sslmode="require"
-)
-
+conn = psycopg2.connect(DATABASE_URL)
 cur = conn.cursor()
 
-print("Connected to Railway PostgreSQL Successfully!")
+print("Connected to Railway PostgreSQL!")
